d92820afc8b626fabbbe5beb35e197b9abff2aca
Moh Lutfi Fauzi <mlfauzi54@gmail.com>
2023-07-06 11:47:05 +0700

    add search in mobile version

--- src/app/component/NavbarMobile.js
+++ src/app/component/NavbarMobile.js
@@ -4,9 +4,10 @@ import { use } from 'react'
 import Image from 'next/image'
 import React, { useState } from 'react'
 import logo from '/public/images/rakcer-logo.png'
-import { HiBars3, HiXMark, HiChevronDown, HiChevronUp } from 'react-icons/hi2'
+import { HiBars3, HiXMark, HiChevronDown, HiChevronUp, HiMagnifyingGlass } from 'react-icons/hi2'
 import Link from 'next/link'
 import { getMenuPrimary } from '../../../lib/query'
+import Search from './Search'
 
 
 export async function allMenuPrimary() {
@@ -20,6 +21,7 @@ const menuPrimaryPromise = getMenuPrimary()
 const NavbarMobile = () => {
     const [toggle, setToggle] = useState(false)
     const [toggleSubMenu, setToggleSubMenu] = useState(false)
+    const [toggleSearch, setToggleSearch] = useState(false)
     const menus = use(menuPrimaryPromise)
 
     const handleToggle = () => {
@@ -30,22 +32,42 @@ const NavbarMobile = () => {
         setToggleSubMenu(!toggleSubMenu);
     };
 
+    const handleToggleSearch = () => {
+        setToggleSearch(!toggleSearch);
+    };
+
     return (
         <>
-            <div className='sticky top-0 z-10 bg-white border shadow-sm p-4 flex md:hidden'>
+            <div className='sticky top-0 z-10 bg-white border shadow-sm p-4 flex justify-between md:hidden'>
                 {
                     toggle ?
                         <HiXMark className="text-gray-700 text-4xl cursor-pointer" onClick={handleToggle} />
                         :
                         <HiBars3 className="text-gray-700 text-4xl cursor-pointer" onClick={handleToggle} />
                 }
-                <Link href='/' className='m-auto'>
+                <Link href='/'>
                     <Image src={logo} width={172} height={40} style={{ objectFit: 'cover' }} alt='logo rakcer' />
                 </Link>
+                {
+                    toggleSearch ?
+                        <HiXMark className="text-gray-700 text-4xl cursor-pointer" onClick={handleToggleSearch} />
+                        :
+                        <HiMagnifyingGlass className="text-gray-700 text-4xl cursor-pointer" onClick={handleToggleSearch} />
+
+                }
             </div>
+
+            {
+                toggleSearch ?
+                    <div className='sticky top-16 z-10 w-full bg-red-500 p-2 md:hidden'>
+                        <Search />
+                    </div> :
+                    null
+            }
+
             {
                 toggle ?
-                    <nav className='sticky top-16 z-10 md:hidden'>
+                    <nav className={`sticky  ${toggleSearch ? 'top-28' : 'top-16'} z-10 md:hidden`}>
                         {
                             menus.map((menu) => {
                                 if (menu.children.length === 0) {
--- src/app/component/Search.js
+++ src/app/component/Search.js
@@ -17,7 +17,7 @@ const Search = () => {
     }
 
     return (
-        <form className="relative w-80" onSubmit={onSearch}>
+        <form className="relative w-full md:w-80" onSubmit={onSearch}>
             <input className='border border-gray-300 rounded-full px-4 py-1 w-full' type='text' placeholder='Cari Berita ...' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
             <AiOutlineSearch className="absolute top-0 bottom-0 w-6 h-6 my-auto right-3 shadow-black" />
         </form>
