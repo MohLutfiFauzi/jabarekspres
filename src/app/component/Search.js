7b091c5b91c7d6c5463311755dbfcb63022d5f03
Moh Lutfi Fauzi <mlfauzi54@gmail.com>
2023-07-07 15:16:24 +0700

    change icon to button in searchbar

--- src/app/component/Search.js
+++ src/app/component/Search.js
@@ -19,7 +19,9 @@ const Search = () => {
     return (
         <form className="relative w-full md:w-80" onSubmit={onSearch}>
             <input className='border border-gray-300 rounded-full px-4 py-1 w-full' type='text' placeholder='Cari Berita ...' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
-            <AiOutlineSearch className="absolute top-0 bottom-0 w-6 h-6 my-auto right-3 shadow-black" />
+            <button type="submit" className="absolute top-0 bottom-0 my-auto right-3 bg-transparent border-none cursor-pointer">
+                <AiOutlineSearch className="w-6 h-6 shadow-black" />
+            </button>
         </form>
     )
 }
